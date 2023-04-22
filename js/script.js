598d97a1ac7602553d65aa16fdffe625a41d84d0
and <andersonvittor1801@gmail.com>
2023-04-22 18:18:57 -0300

    Terceiro commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+// funcoes auxiliares e uteis
+
+const seleciona = (elemento) => document.querySelector(elemento)
+const selecionaTodos = (elemento) => document.querySelectorAll(elemento)
+
+const preencherDadosDosMilkshakes = (milkshakeItem, item, index) => {
+    milkshakeItem.setAttribute('data-key', index)
+    milkshakeItem.querySelector('.milk-item--img img').src = item.img
+    milkshakeItem.querySelector('.milk-item--price').innerHTML = item.prices
+    milkshakeItem.querySelector('.milk-item--name').innerHTML = item.name
+    milkshakeItem.querySelector('.milk-item--desc').innerHTML = item.description
+}
+
+// Mapear os Milkshakes
+
+MilkshakeJson.map((item, index) => {
+    console.log(item)
+
+    let milkshakeItem = document.querySelector('.models .milk-item').cloneNode(true) // cloneNode faz uma copia dos elementos
+
+    // agora que foi clonado precisamos colocar na main e fazer aparecer na tela
+    seleciona('.milkshake-area').append(milkshakeItem)
+
+    //hora de preencher os dados de cada milkshake
+    preencherDadosDosMilkshakes(milkshakeItem, item, index)
+})
---
+++ milkshakes.js
@@ -0,0 +1,5 @@
+let MilkshakeJson = [
+    { id: 1, name: 'Tradicional', img: 'img/tradicional.png', prices: [14.00, 17.00, 19.00], sizes: ['300', '400', '500'], description: 'Milkshake tradicional com leite, chantily e sorvete' },
+    { id: 2, name: 'Baunilha', img: 'img/baunilha.png', prices: [15.00, 18.00, 20.00], sizes: ['300', '400', '500'], description: 'Sorvete, leite, chantily e baunilha' },
+    { id: 3, name: 'Morango', img: 'img/morango.png', prices: [20.00, 25.00, 30.00], sizes: ['300', '400', '500'], description: 'Sorvete, leite, chantily e morangos batidos' }
+]
