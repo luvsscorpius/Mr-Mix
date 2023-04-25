a3571557a6e20b6cbe87a802e7b3bd9b66b9fadf
and <andersonvittor1801@gmail.com>
2023-04-25 20:18:20 -0300

    Decimo commit

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@
 let modalKey = 0
 
 // variavel para controlar a quantidade inicial de pizzas no modal
-let qtMilkshakes = 1
-let qtCascao = 1
-let qtKids = 1
+let qtProdutos = 1
 
+let cart = []
 
 // funcoes auxiliares e uteis
 
@@ -47,20 +46,6 @@ const botoesFechar = () => {
     })
 }
 
-const pegarKeyMilk = (e) => {
-    let key = e.target.closest('.milk-item').getAttribute('data-key')
-    console.log('Milkshake Clicado ' + key)
-    console.log(produtosJson[key])
-
-    // garantir que a quantidade inicial de milkshakes é 1 
-    qtMilkshakes = 1
-
-    //para manter a informação de qual milkshake foi clicado
-    modalKey = key
-
-    return key
-}
-
 const preencherDadosDosMilkshakes = (milkshakeItem, item, index) => {
     milkshakeItem.setAttribute('data-key', index)
     milkshakeItem.querySelector('.milk-item--img img').src = item.img
@@ -92,10 +77,130 @@ const preencheDadosModal = (item) => {
     seleciona('.milkshakeInfo--desc').innerHTML = item.description
 }
 
-// Mapear os Milkshakes
-const milkshakes = produtosJson.filter((produto) => produto.type === "Milkshake")
-console.log(milkshakes)
-milkshakes.map((item, index) => {
+const adicionarNoCarrinho = () => {
+    seleciona('.milkshakeInfo--addButton').addEventListener('click', () => {
+        console.log('Adicionei')
+
+        //pegar dados da janela modal atual
+        // qual produto? pegue o modalkey para usar produtosJson[modalkey]
+        console.log('Produto' + modalKey)
+
+        //tamanho
+        let size = seleciona('.milkshake--size.selected').getAttribute('data-key')
+        console.log('Tamanho' + size)
+
+        //quantidade
+        console.log('Quant' + qtProdutos)
+
+        // preco 
+        let price = seleciona('.milkshakeInfo--actualPrice').innerHTML.replace('R$&nbsp', '')
+
+        //crie um identificador que junte id e tamanho
+        // concatene as duas informações separadas por um símbolo
+        let identificador = produtosJson[modalkey].id + 't' + size
+
+        // antes de adicionar verifique se ja tem aquele codigo e tamanho
+        // para adicionarmos a quantidade
+        let key = cart.findIndex((item) => item.identificador == identificador)
+        console.log(key)
+
+        if (key > -1) {
+            // se encontrar aumente a quantidade
+            cart[key].qt += qtProdutos
+        } else {
+            // adicionar objeto pizza no carrinho
+            let pizza = {
+                identificador,
+                id: produtosJson[modalkey].id,
+                size,
+                qt: qtProdutos,
+                price: parseFloat(price)
+            }
+            cart.push(produto)
+            console.log(produto)
+            console.log('Sub total R$' + (qtProdutos * produto.price).toFixed[2])
+        }
+        fecharModal()
+    })
+}
+
+
+const pegarKey = (e) => {
+    let key = e.target.closest('.milk-item').getAttribute('data-key')
+    console.log('Milkshake Clicado ' + key)
+    console.log(produtosJson[key])
+
+    // garantir que a quantidade inicial de milkshakes é 1 
+    qtMilkshakes = 1
+
+    //para manter a informação de qual milkshake foi clicado
+    modalKey = key
+
+    return key
+}
+
+const preencherTamanhos = (key) => {
+    // tirar a selecao de tamanho atual e selecionar o tamanho grande
+    seleciona('.milkshake--size.selected').classList.remove('selected')
+
+    // selecionar todos os tamanhos
+    selecionaTodos('.milkshake--size').forEach((size, sizeIndex) => {
+        // selecionar o tamanho grande
+        (sizeIndex == 2) ? size.classList.add('selected') : ''
+        size.querySelector('span').innerHTML = produtosJson[key].sizes[sizeIndex]
+    })
+}
+
+const escolherTamanhoPreco = (key) => {
+    // Ações nos botões de tamanho
+    // Selecionar todos os tamanhos
+    selecionaTodos('.milkshakeInfo--size').forEach((size, sizeIndex) => {
+        size.addEventListener('click', (e) => {
+            //clicou em um item, tira a seleção dos outros e marca o que voce selecionou
+            // tirar a seleção de tamanho atual e selecionar o tamanho grande
+            seleciona('.milkshakeInfo--size.selected').classList.remove('selected')
+            // marcar o que voce clicou, ao inves de usar o e.target use size, pois ele é nosso item dentro do loop
+            size.classList.add('selected')
+
+            //mudar o preço de acordo com o tamanho
+            seleciona('milkshakeInfo--actualPrice').innerHTML = formatoReal(produtosJson[key].price[sizeIndex])
+        })
+    })
+}
+
+const abrirCarrinho = () => {
+    console.log('Qtd de itens no carrinho' + cart.length)
+    if (cart.length > 0) {
+        //mostrar o carrinho
+        seleciona('aside').classList.add('show')
+        seleciona('header').style.display = 'flex' // mostrar barra superior
+    }
+
+    // exibir o aside do carrinho no mobile
+    seleciona('.menu-openner').addEventListener('click', () => {
+        if (cart.length > 0) {
+            seleciona('aside').classList.add('show')
+            seleciona('aside').style.left = '0'
+        }
+    })
+}
+
+const mudarQuantidade = () => {
+    // Ações nos botões + - da janela modal
+    seleciona('.milkshakeInfo--qtmais').addEventListener('click', () => {
+        qtProdutos++
+        seleciona('.milkshakeInfo--qt').innerHTML = qtProdutos
+    })
+
+    seleciona('.milkshake--qtmenos').addEventListener('click', () => {
+        if (qtProdutos > 1) {
+            qtProdutos--
+            seleciona('.milkshakeInfo--qt').innerHTML = qtProdutos
+        }
+    })
+}
+
+produtosJson.map((item, index) => {
     console.log(item)
 
     let milkshakeItem = document.querySelector('.models .milk-item').cloneNode(true) // cloneNode faz uma copia dos elementos
@@ -111,7 +216,10 @@ milkshakes.map((item, index) => {
         e.preventDefault() // faz o a (link) nao fazer o padrao que é dar refresh
         console.log('Clicou no milkshake')
 
-        let chave = pegarKeyMilk(e)
+        let chave = pegarKey(e)
+
+        //definir quantidade inicial como 1
+        seleciona('.milkshakeInfo--qt').innerHTML = qtProdutos
 
         // quando clicar em um milkshake a janela modal abre
         abrirModal()
@@ -122,72 +230,12 @@ milkshakes.map((item, index) => {
         // preencher os dados dos vetores no modal 
         preencheDadosModal(item)
 
-    })
-})
+        // para mudar a quantidade de milkshakes
+        mudarQuantidade()
 
-// Mapear os cascões
-const cascao = produtosJson.filter((produto) => produto.type === "Cascao")
-console.log(cascao)
-cascao.map((item, index) => {
-    console.log(item)
-
-    let cascaoItem = document.querySelector('.models .milk-item').cloneNode(true) // clona o elemento
-
-    // agora que foi clonado vamos colocar na main visualmente
-    seleciona('.cascao-area').append(cascaoItem)
-
-    // agora que ja está aparecendo visualmente simbora preencher os dados de acordo com o script
-    preencherDadosDosCascao(cascaoItem, item, index)
-
-    // agora queremos saber se o cascao foi clicado
-    cascaoItem.querySelector('.milk-item a').addEventListener('click', (e) => {
-        e.preventDefault()  // faz com que o link não faça o padrão que seria dar o refresh
-        console.log('Clicou no cascão')
-
-        let chave = pegarKeyMilk(e)
-
-        // Para abrir o modal quando for clicado em algum cascão
-        abrirModal()
-
-        // para fechar o modal quando for clicado em algum botao de cancelar/voltar
-        botoesFechar()
-
-        // preencher os dados dos vetores no modal 
-        preencheDadosModal(item)
-
-    })
-
-})
-
-//Mapear a area kids
-const kids = produtosJson.filter((produto) => produto.type === "Kids")
-console.log(kids)
-kids.map((item, index) => {
-    console.log(item)
-
-    let kidsItem = document.querySelector('.models .milk-item').cloneNode(true) //clona o elemento
-
-    // depois de clonado tem que aparecer na tela né?
-    seleciona('.kids-area').append(kidsItem)
-
-    // e agora preencher os dados no que foi clonado
-    preencherDadosKids(kidsItem, item, index)
-
-    // agora precisamos saber se algum produto da area kids foi clicado
-    kidsItem.querySelector('.milk-item a').addEventListener('click', (e) => {
-        e.preventDefault() // faz com que não faça o padrao que no caso do a seria dar refresh
-        console.log('Clicou em algum produto da area kids')
-
-        let chave = pegarKeyMilk(e)
-
-        // Para abrir o modal quando for clicado em algum cascão
-        abrirModal()
-
-        // para fechar o modal quando for clicado em algum botao de cancelar/voltar
-        botoesFechar()
-
-        // preencher os dados dos vetores no modal 
-        preencheDadosModal(item)
+        // carinho
+        adicionarNoCarrinho()
 
     })
 })
+
